62520a298ab389ddf9a74ea54bdf1dc43a2b78ae
Jos Ahrens <buughost@gmail.com>
2023-09-04 18:49:03 +0200

    improve rendering of posts

--- src/components/PostPreview.vue
+++ src/components/PostPreview.vue
@@ -6,7 +6,12 @@ defineProps(['post']);
     <v-card>
         <v-card-title>{{ post.title }}</v-card-title>
         <v-card-subtitle>Published at: {{ post.published_at }}</v-card-subtitle>
-        <v-card-text>{{ post.headline.join(' ') }}...</v-card-text>
+        <v-card-text>
+            <p v-for="(paragraph, index) in post.headline" :key="index">
+                {{ paragraph }}
+            </p>
+            <p>...</p>
+        </v-card-text>
         <v-card-actions>
             <v-btn>
                 <z-link :href="post.uri" icon="mdi-open-in-new" :details="`Opens blog post ${post.title}`">
